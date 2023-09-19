1cc7abec6f2a0c3aa0e852803185acc2fb13f17b
Sarah E <seatherly.prsvr@gmail.com>
2023-09-19 16:32:36 -0500

    finished

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('p').style.color = 'lightblue';
+document.querySelector('h1').style.fontSize = '48px';
+document.querySelector('p').innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+document.getElementById('item-13').style.opacity = .5;
+document.getElementById('item-3').innerHTML = 'I say, "Hi!"';
+document.getElementById('firstimg').src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+document.getElementById('firstimg').style.height = '300px';
+document.getElementById('maskman').src = 'https://wallpapers.com/images/high/cool-picture-mask-man-jqswpm4svdc378fl.webp';
+document.getElementById('maskman').style.height = '300px';
+document.getElementById('item-16').className = 'item';
+document.getElementById('item-16').innerHTML = "Won't get fooled again."
